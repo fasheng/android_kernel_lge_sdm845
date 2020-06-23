da36ea9eb051d61c5ecac3fe69f8c49e55910843
Xu Fasheng <fasheng@fasheng.info>
2020-06-23 09:53:47 +0800

    Add build_anykernel.sh

---
+++ build_anykernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+version="$(./kernelversion.sh | grep -v make)"
+echo "Kernel Version is ${version}"
+echo ""
+
+if [[ ! -e releases ]]; then
+	mkdir releases
+fi
+
+cp out/arch/arm64/boot/Image.gz-dtb anykernel3/
+cd anykernel3
+zip -r9 nethunter-TNOKernel-v$version.zip * -x .git README.md *placeholder
+mv nethunter-TNOKernel-v$version.zip ../releases
+rm Image.gz-dtb
